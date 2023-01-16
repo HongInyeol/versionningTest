f29d0ec05896b3ff20ba58173f3b49bb3c367237
inyeol.hong <inyeol.hong@2bytescorp.com>
2023-01-16 17:53:40 +0900

    fix : test2

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,11 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return <div className="App">TEST</div>;
+  return (
+    <>
+      <div className="App">TEST</div>;<div className="App">TEST2</div>
+    </>
+  );
 }
 
 export default App;
