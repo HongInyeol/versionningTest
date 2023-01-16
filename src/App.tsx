0e565d301d48f04614a66201bb7658a859cee1eb
inyeol.hong <inyeol.hong@2bytescorp.com>
2023-01-16 18:27:46 +0900

    Test 8

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <>
-      <div className="App">TEST</div>;<div className="App">TEST2</div>
+      <div className="App">TEST</div>;<div className="App">TEST3</div>
     </>
   );
 }
