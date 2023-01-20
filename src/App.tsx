e77fb3d1cb1ed6581fd4ba1493fcebd50def576b
inyeol.hong <inyeol.hong@2bytescorp.com>
2023-01-20 10:16:46 +0900

    t

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import './App.css';
 
 function App() {
+  const nodeEnv = process.env.NODE_ENV;
   return (
     <>
       <div className="App">TEs2ST</div>;<div className="App">TEST4443ttt</div>
+      <div className="App">{nodeEnv}</div>
     </>
   );
 }
